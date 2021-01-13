dc422cc385f069bc5ac7c7311a08759e1c41df75
Zhenyi He <snowymohehe@gmail.com>
2021-01-12 21:45:37 -0500

    :confused:

--- load_llff.py
+++ load_llff.py
@@ -167,7 +167,8 @@ def recenter_poses(train_poses, poses, c2w=None):
 
     poses_ = poses+0
     bottom = np.reshape([0,0,0,1.], [1,4])
-    c2w = poses_avg(train_poses)
+    if c2w == None:
+        c2w = poses_avg(train_poses)
     c2w = np.concatenate([c2w[:3,:4], bottom], -2)
     bottom = np.tile(np.reshape(bottom, [1,1,4]), [poses.shape[0],1,1])
     poses = np.concatenate([poses[:,:3,:4], bottom], -2)
