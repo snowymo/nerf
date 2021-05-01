de50edff6850083a7e1e009a0d76e5ab40c80907
Zhenyi He <snowymohehe@gmail.com>
2021-04-30 22:18:13 -0400

    :memo: add script and config files for running on hpc

---
+++ nerf-stones.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=nerfStones
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=80GB
+#SBATCH --time=8:00:00
+#SBATCH --gres=gpu:1
+
+module purge
+
+source /scratch/zh719/anaconda3/etc/profile.d/conda.sh
+export PATH=/scratch/zh719/anaconda3/bin:$PATH
+conda activate nerf
+
+cd /scratch/zh719/nerf
+
+python run_nerf.py --config config_stones_0430.txt
+
