1e648eac2ecdc72747e6277fcb385da8a73869f3
Zhenyi He <snowymohehe@gmail.com>
2021-05-15 08:30:36 -0400

    :beer:

---
+++ nerf-lobby-ndc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=lobbyNDCTest
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=128GB
+#SBATCH --time=23:00:00
+#SBATCH --gres=gpu:1
+
+module purge
+
+source /scratch/zh719/anaconda3/etc/profile.d/conda.sh
+export PATH=/scratch/zh719/anaconda3/bin:$PATH
+conda activate nerf
+
+cd /scratch/zh719/nerf
+
+python run_nerf.py --config config_lobby_0506_160deg.txt --NDC True
+
